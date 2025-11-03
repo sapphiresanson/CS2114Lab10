3f9a8ad994286d4d327f50ec521294b000091135
sapphiresanson <sapphiresanson@vt.edu>
2025-11-02 23:52:53 -0500

    add a test method for size
    add a test method for size

--- src/doublylinkedlist/DLListTest.java
+++ src/doublylinkedlist/DLListTest.java
@@ -386,4 +386,15 @@ public class DLListTest
 
     }
 
+
+    public void testSize2()
+    {
+        list.add("1");
+        list.add("10");
+        list.add("100");
+        assertEquals(3, list.size());
+        list.clear();
+        assertEquals(0, list.size());
+    }
+
 }
